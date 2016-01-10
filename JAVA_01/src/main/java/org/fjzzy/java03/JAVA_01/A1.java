c27898a4db2e07652b256aab31c4172d3d020e95
linyuhua1123 <513924497@qq.com>
2016-01-10 16:17:46 +0800

    lyh2

--- JAVA_01/src/main/java/org/fjzzy/java03/JAVA_01/A1.java
+++ JAVA_01/src/main/java/org/fjzzy/java03/JAVA_01/A1.java
@@ -5,7 +5,7 @@ public class A1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("linyuhua  linyuhua1");
-		System.out.println("chenkang");
+		System.out.println("linyuhua 2");
 		System.out.println("chenyaohesadasd");
 	}
 
