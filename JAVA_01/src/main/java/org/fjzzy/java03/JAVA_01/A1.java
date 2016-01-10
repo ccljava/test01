760408401148bc57508f8baccbb9d22436c6ea71
Seven_Y <cyhtdawh@qq.com>
2016-01-10 16:16:17 +0800

    chenyaohe 1

--- JAVA_01/src/main/java/org/fjzzy/java03/JAVA_01/A1.java
+++ JAVA_01/src/main/java/org/fjzzy/java03/JAVA_01/A1.java
@@ -5,7 +5,7 @@ public class A1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("linyuhua  dadadada");
-		System.out.println("chenkang");
+		System.out.println("chenkang chenyaohe");
 		System.out.println("chenyaohe");
 	}
 
