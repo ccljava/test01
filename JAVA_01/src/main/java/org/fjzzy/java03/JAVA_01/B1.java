0f4c5548c98abf4f5305c65d93ae991da45f69a3
Seven_Y <cyhtdawh@qq.com>
2016-01-10 15:39:35 +0800

    CYH  CREATE

---
+++ JAVA_01/src/main/java/org/fjzzy/java03/JAVA_01/B1.java
@@ -0,0 +1,10 @@
+package org.fjzzy.java03.JAVA_01;
+
+public class B1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JAVA_01/src/main/java/org/fjzzy/java03/JAVA_01/B3.java
@@ -0,0 +1,10 @@
+package org.fjzzy.java03.JAVA_01;
+
+public class B3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
